4bcab6c9f306352cb9b47d89554c64577909ac84
breysijara <breysijara@gmail.com>
2018-07-04 13:31:45 -0500

    Initial commit

---
+++ tests/CalculateTest.php
@@ -0,0 +1,65 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+
+final class CalculateTest extends TestCase{
+
+	public function testAdd():void{
+		include_once "src/calculate.php";
+		$calculate=new Calculate();
+		$this->assertEquals(  $calculate->add(0,1), 1);
+	}
+
+	public function testDivide():void{
+
+		$calculate=new Calculate();
+		$this->assertEquals(  $calculate->divide(5,0), "Error Division entre 0" );
+	}
+
+	public function testDividee():void{
+
+		$calculate=new Calculate();
+		$this->assertEquals(  $calculate->divide(12,3), 5);
+	}
+
+
+	public function testDivideee():void{
+
+		$calculate=new Calculate();
+		$this->assertEquals(  $calculate->divide(12,3), 4);
+	}
+
+	public function testarrayAdded():void{
+		$calculate=new Calculate();
+		$array=[];
+		$this->assertEquals(  $calculate->arrayAdded($array), 0);
+	}
+	public function testarrayAdded2():void{
+		$calculate=new Calculate();
+		$array=[5,4,8];
+		$this->assertEquals(  $calculate->arrayAdded($array), 17);
+	}
+	public function testarrayAdded3():void{
+		$calculate=new Calculate();
+		$array=[5.6,6.2,4.2];
+		$this->assertEquals(  $calculate->arrayAdded($array), 16);
+	}
+	public function testarrayAdded4():void{
+		$calculate=new Calculate();
+		$array=['4','4',5];
+		$this->assertEquals(  $calculate->arrayAdded($array), 13);
+	}
+
+	public function testarrayAdded5():void{
+		$calculate=new Calculate();
+		$array=['cuatro','4',5];
+		$this->assertEquals(  $calculate->arrayAdded($array), 9);
+	}
+
+	public function testarrayAdded6():void{
+		$calculate=new Calculate();
+		$array=null;
+		$this->assertEquals(  $calculate->arrayAdded($array), 0);
+	}
+}
+?>
